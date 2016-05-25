92bccf72bcb00836244316a336fdaae6b79ff936
Artūrs Liepiņš <arturs.liepins@accenture.com>
2016-05-25 17:10:27 +0300

    Change base URL to http

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'https://devops.lv'
+SITEURL = 'http://devops.lv'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
